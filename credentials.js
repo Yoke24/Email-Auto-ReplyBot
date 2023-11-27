a25d0c66931217f6a7387c562983433888846fb3
Yoke24 <90189373+Yoke24@users.noreply.github.com>
2023-11-27 20:21:36 +0530

    first commit

---
+++ credentials.js
@@ -0,0 +1,10 @@
+/*
+ How this obtained ? ->see the readme.md file 
+*/
+const CLIENT_ID =
+  "773291900345-3a31t80hd9sqhk470u624vun7jpqkkqk.apps.googleusercontent.com";
+const CLEINT_SECRET = "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";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN =
+  "1//04B9xLh-0rQqHCgYIARAAGAQSNwF-L9Ir86sDbTQWBqoH4nmtF0ASHACIyiN-upNGpRD-ueCv7qVt1iAybB1nErS4NFQQFFwdszM";
+module.exports = { CLIENT_ID, CLEINT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
